603b23c53919453c65caf943d1f404c7a2f251c4
Priyanka Chatterjee <priyankachatterjee9075@gmail.com>
2022-01-25 01:25:16 +0530

    aframe added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'aframe';
+// @ts-ignore
+import { Entity, Scene } from "aframe-react";
+// import logo from './logo.svg';
+import pano from './assets/pano.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Scene primitive="a-scene" vr-mode-ui="enabled: true">
+          <Entity primitive="a-sky" src= {pano} rotation="0 0 0"></Entity>
+          <Entity primitive="a-camera">
+          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+        </Entity>
+      </Scene>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +28,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
